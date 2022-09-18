f74691b0ba8b6011f256d0c60f29bababebc210e
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-09-18 21:36:57 +0700

    Menu

---
+++ app/src/main/java/com/kelompok_15/tb_ptb/ListMahasiswaActivity.java
@@ -0,0 +1,13 @@
+package com.kelompok_15.tb_ptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListMahasiswaActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_mahasiswa);
+    }
+}
--- app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
@@ -2,13 +2,28 @@ package com.kelompok_15.tb_ptb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        btn = findViewById(R.id.login);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(LoginActivity.this, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }

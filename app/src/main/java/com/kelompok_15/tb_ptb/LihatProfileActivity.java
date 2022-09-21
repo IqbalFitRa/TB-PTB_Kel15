d3d3682ad663249d824db4a2f4a44d069c33c1f4
jahrosuroya <jahrosuroya3@gmail.com>
2022-09-21 13:49:49 +0700

    first commit

---
+++ app/src/main/java/com/kelompok_15/tb_ptb/LihatProfileActivity.java
@@ -0,0 +1,14 @@
+package com.kelompok_15.tb_ptb;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LihatProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lihat_profile);
+    }
+}

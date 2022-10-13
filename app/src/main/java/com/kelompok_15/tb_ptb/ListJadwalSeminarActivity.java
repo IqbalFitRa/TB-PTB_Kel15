6fa953e641c2e5eee5d2bd7f2f66cfec6863a173
Daribsan <dariyarig@gmail.com>
2022-10-13 19:25:26 +0700

    yang bagian akhirr

--- app/src/main/java/com/kelompok_15/tb_ptb/ListJadwalSeminarActivity.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/ListJadwalSeminarActivity.java
@@ -2,13 +2,28 @@ package com.kelompok_15.tb_ptb;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ListJadwalSeminarActivity extends AppCompatActivity {
+
+    Button menambahkan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list_jadwal_seminar);
+
+        menambahkan = findViewById(R.id.buttonsidang1);
+        menambahkan.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent menambahkan = new Intent(ListJadwalSeminarActivity.this, rekomendasiSidangActivity.class);
+                startActivity(menambahkan);
+
+            }
+        });
     }
 }

3bd71313aee75c3dae8a3d32cb7d66cee36ef6ee
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-11-22 09:27:43 +0700

    Tipistipis v2 :(

--- app/src/main/java/com/kelompok_15/tb_ptb/retrofit/RetrofitClient.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/retrofit/RetrofitClient.java
@@ -7,14 +7,29 @@ import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitClient {
 
-    public String API_BASE_URL = "";
+    private static  MainInterface service;
+    public static MainInterface getService() {
+
+        if (service == null) {
+            String API_BASE_URL = "";
+
+            OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
+            Retrofit.Builder builder = new Retrofit.Builder().baseUrl(API_BASE_URL).addConverterFactory(GsonConverterFactory.create());
+            Retrofit retrofit = builder.client(httpClient.build()).build();
+
+            service = retrofit.create(MainInterface.class);
+        }
+        return service;
+
+    }
+    /*String API_BASE_URL = "";
 
     OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
     Retrofit.Builder builder = new Retrofit.Builder().baseUrl(API_BASE_URL).addConverterFactory(GsonConverterFactory.create());
     Retrofit retrofit = builder.client(httpClient.build()).build();
 
 
-    /*  @NonNull
+      @NonNull
     public static ... ...(){
         String API_BASE_URL = "";
     }*/

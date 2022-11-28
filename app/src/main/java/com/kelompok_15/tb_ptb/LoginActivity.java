04984756ce56f7ddbc032d61823972f6649f7046
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-11-28 21:16:42 +0700

    Tipistipis v5 :(

--- app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.core.app.NotificationManagerCompat;
 
 import android.app.NotificationChannel;
 import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
@@ -15,6 +16,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.kelompok_15.tb_ptb.retrofit.LoginBody;
+import com.kelompok_15.tb_ptb.retrofit.LoginResponse;
+import com.kelompok_15.tb_ptb.retrofit.MainInterface;
+import com.kelompok_15.tb_ptb.retrofit.RetrofitClient;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -30,9 +40,9 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
 
         notifMan = NotificationManagerCompat.from(this);
+        createNotificationChannel();
 
         Button login =(Button) findViewById(R.id.login);
-
         login.setOnClickListener(new View.OnClickListener()
         {
             @Override
@@ -41,29 +51,40 @@ public class LoginActivity extends AppCompatActivity {
                 TextView username = (TextView) findViewById(R.id.emailLogin);
                 TextView password = (TextView) findViewById(R.id.passwordLogin);
 
-                if(username.getText().toString().equals("Dosen") && password.getText().toString().equals("dosen")){
-                    Intent login = new Intent(LoginActivity.this, MenuActivity.class);
-                    startActivity(login);
-                }else
-                    Toast.makeText(LoginActivity.this, "Email/Password Wrong", Toast.LENGTH_SHORT).show();
+               /* MainInterface mainInterface = RetrofitClient.getService();
+                Call<LoginResponse> call = mainInterface.postLogin(username.getText().toString(),password.getText().toString());
+                call.enqueue(new Callback<LoginResponse>() {
+                    @Override
+                    public void onResponse(Call<LoginResponse> call, Response<LoginResponse> response) { */
+                        if(username.getText().toString().equals("Dosen") && password.getText().toString().equals("dosen")){
+                            Intent login = new Intent(LoginActivity.this, MenuActivity.class);
+                            startActivity(login);
+
+
+                    //PendingIntent resultPendingIntent =
+                      //      stackBuilder.getPendingIntent(0,
+                       //             PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE);
+                            NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this, CHANNEL_ID)
+                                    .setSmallIcon(R.drawable.logounand)
+                                    .setContentTitle("Notifikasi Login")
+                                    .setContentText("Login berhasil")
+                                    .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                            notifMan.notify(101, builder.build());
+
+                        }else
+                            Toast.makeText(LoginActivity.this, "Email/Password Wrong", Toast.LENGTH_SHORT).show();
+
+                    /*}
+
+                    @Override
+                    public void onFailure(Call<LoginResponse> call, Throwable t) {
+                        Toast.makeText(LoginActivity.this, "Gagal", Toast.LENGTH_SHORT).show();
+                    }
+                });*/
+
             }
         });
-
-        Button notiffr =(Button) findViewById(R.id.notif_Fr);
-        notiffr.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this, CHANNEL_ID)
-                        .setSmallIcon(R.drawable.logounand)
-                        .setContentTitle("Notifikasi Login")
-                        .setContentText("Login berhasil")
-                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
-
-                notifMan.notify(101, builder.build());
-            }
-        });
-
     }
 
     //channel
--- app/src/main/java/com/kelompok_15/tb_ptb/retrofit/MainInterface.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/retrofit/MainInterface.java
@@ -2,10 +2,13 @@ package com.kelompok_15.tb_ptb.retrofit;
 
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.POST;
 
 public interface MainInterface {
 
     @POST("api/login")
-    Call<LoginResponse> postLogin (@Body LoginBody loginBody);
+    Call<LoginResponse> postLogin (
+            @Field("email") String email,
+            @Field("password") String password);
 }

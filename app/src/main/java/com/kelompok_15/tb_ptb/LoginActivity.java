60485239a91fced8d38eb628f347e8d8b1b6db86
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-11-28 18:55:33 +0700

    Tipistipis v4 :(

--- app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
@@ -1,8 +1,13 @@
 package com.kelompok_15.tb_ptb;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
@@ -13,14 +18,18 @@ import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String CHANNEL_ID = "ChannelNotif";
     //Button login;
     //String userName = "Dosen";
     //String password = "dosen";
+    private NotificationManagerCompat notifMan;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        notifMan = NotificationManagerCompat.from(this);
 
         Button login =(Button) findViewById(R.id.login);
 
@@ -39,5 +48,32 @@ public class LoginActivity extends AppCompatActivity {
                     Toast.makeText(LoginActivity.this, "Email/Password Wrong", Toast.LENGTH_SHORT).show();
             }
         });
+
+        Button notiffr =(Button) findViewById(R.id.notif_Fr);
+        notiffr.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                NotificationCompat.Builder builder = new NotificationCompat.Builder(LoginActivity.this, CHANNEL_ID)
+                        .setSmallIcon(R.drawable.logounand)
+                        .setContentTitle("Notifikasi Login")
+                        .setContentText("Login berhasil")
+                        .setPriority(NotificationCompat.PRIORITY_DEFAULT);
+
+                notifMan.notify(101, builder.build());
+            }
+        });
+
+    }
+
+    //channel
+    private void createNotificationChannel() {
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+
+            NotificationChannel channel = new NotificationChannel(CHANNEL_ID, "Login Notifikasi", NotificationManager.IMPORTANCE_HIGH);
+            channel.setDescription("This is Description for Login Notification");
+            notifMan.createNotificationChannel(channel);
+        }
     }
 }

e703164a2cb018b830b6083f694c3f03eba1b0cc
IqbalFitRa <89772053+IqbalFitRa@users.noreply.github.com>
2022-12-12 23:00:07 +0700

    fireBase progress

--- app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
+++ app/src/main/java/com/kelompok_15/tb_ptb/LoginActivity.java
@@ -1,5 +1,6 @@
 package com.kelompok_15.tb_ptb;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.NotificationCompat;
 import androidx.core.app.NotificationManagerCompat;
@@ -11,6 +12,7 @@ import android.app.TaskStackBuilder;
 import android.content.Intent;
 import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.widget.Button;
@@ -18,6 +20,9 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.messaging.FirebaseMessaging;
 import com.kelompok_15.tb_ptb.retrofit.LoginBody;
 import com.kelompok_15.tb_ptb.retrofit.LoginResponse;
 import com.kelompok_15.tb_ptb.retrofit.MainInterface;
@@ -29,6 +34,7 @@ import retrofit2.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
+    private static final String TAG = "LoginActivity-Debug";
     private static final String CHANNEL_ID = "ChannelNotif";
     //Button login;
     //String userName = "Dosen";
@@ -39,6 +45,24 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FirebaseMessaging.getInstance().getToken()
+                .addOnCompleteListener(new OnCompleteListener<String>() {
+                    @Override
+                    public void onComplete(@NonNull Task<String> task) {
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "Fetching FCM registration token failed", task.getException());
+                            return;
+                        }
+
+                        // Get new FCM registration token
+                        String token = task.getResult();
+
+                        // Log and toast
+                        Log.d(TAG, token);
+                        Toast.makeText(LoginActivity.this, token, Toast.LENGTH_SHORT).show();
+                    }
+                });
 
         notifMan = NotificationManagerCompat.from(this);
         createNotificationChannel();
@@ -61,7 +85,7 @@ public class LoginActivity extends AppCompatActivity {
                             Intent login = new Intent(LoginActivity.this, MenuActivity.class);
                             startActivity(login);
 
-                            Intent resultIntent = new Intent(LoginActivity.this, MenuActivity.class);
+                            Intent resultIntent = new Intent(LoginActivity.this, LoginActivity.class);
 
                             TaskStackBuilder stackBuilder = TaskStackBuilder.create(LoginActivity.this);
                             stackBuilder.addNextIntentWithParentStack(resultIntent);
@@ -72,6 +96,8 @@ public class LoginActivity extends AppCompatActivity {
                                     .setSmallIcon(R.drawable.logounand)
                                     .setContentTitle("Notifikasi Login")
                                     .setContentText("Login berhasil")
+                                    .setContentIntent(resultPendingIntent)
+                                    .addAction(R.drawable.logounand,"Lihat",resultPendingIntent)
                                     .setPriority(NotificationCompat.PRIORITY_DEFAULT);
 
                             notifMan.notify(101, builder.build());
